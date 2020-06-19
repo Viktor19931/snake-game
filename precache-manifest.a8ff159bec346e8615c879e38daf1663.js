24685836cba64d95b04bc66bc0d5d8f972243405
Viktor <viktor_hordubey@yahoo.com>
2020-06-19 07:04:13 +0300

    Updates

---
+++ precache-manifest.a8ff159bec346e8615c879e38daf1663.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7983455399a69a4f0f4804f7789bf32a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "174563da3b7495c61952",
+    "url": "/static/js/2.cf2c6bf9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cf2c6bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecdd701ffec666dbdfaf",
+    "url": "/static/js/main.f403d537.chunk.js"
+  },
+  {
+    "revision": "d9a5b028273c76556fc0",
+    "url": "/static/js/runtime-main.ef94caf0.js"
+  }
+]);
